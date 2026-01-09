e6e9b0e69da3563c56fa342007c74e5014f931dc
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-01-09 18:57:03 +0530

    Solved Assignment Question 2

---
+++ Strings/AssignmentQ2.java
@@ -0,0 +1,15 @@
+package Strings;
+
+public class AssignmentQ2 {
+    public static void main(String[] args) {
+        //Question 2 :What will be the output of the following code?
+
+        String str  = "ShradhaDidi";
+        String str1 = "ApnaCollege";
+        String str2 = "ShradhaDidi";
+
+        System.out.println(str.equals(str1) + " " + str.equals(str2));
+
+        //Output : false true
+    }
+}
