9dbdf62c40b4fb04957708cc5e373d8162379876
KuwarGautam-01 <kuwargautam01@gmail.com>
2026-01-09 19:06:08 +0530

    Solved Assignment Question 3

---
+++ Strings/AssignmentQ3.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class AssignmentQ3 {
+    public static void main(String[] args) {
+
+        //Question 3 :What will be the output of the following code?
+
+        String str = "ApnaCollege".replace("l", "");
+        System.out.println(str);
+
+        //Output : ApnaCoege
+    }
+}
